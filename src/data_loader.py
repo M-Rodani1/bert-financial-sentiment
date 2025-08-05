04a68c8faf11d6ef5d2c7ce3efe0f0d89ed3b2c9
M-Rodani1 <rodani484@gmail.com>
2025-08-05 01:54:48 +0100

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,1 +1,15 @@
+import pandas as pd
+def load_data():
+    df = pd.read_csv("data/data.csv")
+    print(df.columns)
+    print(df.shape)
+    print(df['Sentiment'].value_counts())
+    print(df.head())
+    label_map = {"negative": 0,"neutral": 1, "positive":2}
+    df['label'] = df['Sentiment'].map(label_map)
+    print("label distribution:")
+    print(df['label'].value_counts())
+    return df
 
+if __name__ == "__main__":
+    load_data()
