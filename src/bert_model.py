88a8d6093dd80997f0b020c05cc56b0b88a90562
M-Rodani1 <rodani484@gmail.com>
2025-08-05 01:56:21 +0100

    Update bert_model.py

--- src/bert_model.py
+++ src/bert_model.py
@@ -1,1 +1,13 @@
+import torch
+import torch.nn as nn
+from transformers import BertTokenizer, BertForSequenceClassification
 
+class BertSentimentClassifier:
+    def __init__(self,model_name):
+        self.tokenizer = BertTokenizer.from_pretrained(model_name)
+        self.model = BertForSequenceClassification.from_pretrained(model_name,num_labels=3)
+    def predict(self,text,):
+        inputs = self.tokenizer(text,add_special_tokens=True,padding=True,truncation=True,max_length=128,return_tensors="pt")
+        outputs = self.model(**inputs)
+        predictions = torch.argmax(outputs.logits, dim=-1)
+        return predictions
